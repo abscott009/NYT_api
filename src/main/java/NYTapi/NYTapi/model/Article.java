e6ae39c54306af6a005868f402af8ab8518cbc4c
andrewscott <abscott009@gmail.com>
2023-04-18 13:57:52 -0400

    Commented out 'fragments' in index and got 8080 to work.

--- src/main/java/NYTapi/NYTapi/model/Article.java
+++ src/main/java/NYTapi/NYTapi/model/Article.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.print.attribute.standard.Media;
 import java.util.List;
 
 @Getter
